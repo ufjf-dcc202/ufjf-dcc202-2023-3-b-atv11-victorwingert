7f55e00f7c263f26e32e7c76b4327ec44b501b45
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-11-21 22:59:51 -0300

    att lista ordenada

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+import { adicionaNaLista, getLista } from "./src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adcElementoLista("Primeiro");
-adcElementoLista("Segundo");
-adcElementoLista("Terceiro");
+attListaOrdenada();
+
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+
+function adicionaItemDaEntrada(){
+    const valor = pEntrada.textContent;
+    adcElementoLista(valor);
+    pEntrada.textContent = "";
+    attListaOrdenada();
+}
+
+function attListaOrdenada(){
+    const lista = getLista();
+    olItens.innerHTML = "";
+    for(let i = 0; i < lista.length; i++){
+        adcElementoLista(lista[i]);
+    }
+}
 
 function adcElementoLista(texto){
     const li = document.createElement("li");
