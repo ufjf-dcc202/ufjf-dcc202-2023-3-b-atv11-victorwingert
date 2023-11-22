e52dc06723f9093fd13c7b915055210fb0115d8b
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-11-21 22:22:23 -0300

    fiz uma funcao para adicionar elementos na lista

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
+adcElementoLista("Primeiro");
+adcElementoLista("Segundo");
+adcElementoLista("Terceiro");
 
-const li = document.createElement("li");
-li.textContent = "Primeiro";
-olItens.appendChild(li);
+function adcElementoLista(texto){
+    const li = document.createElement("li");
+    li.textContent = texto;
+    olItens.appendChild(li);
+}
